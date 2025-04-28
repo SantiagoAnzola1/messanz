8d3eaa02f3bc54752640eb3c44b9df99067f9f75
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-04-28 16:59:57 -0500

    chore: update ESLint config to avoid unnecessary deploy errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,17 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+
+      "no-var": "off",
     },
   },
 ];
