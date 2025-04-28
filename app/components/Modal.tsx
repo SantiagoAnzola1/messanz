8acdba73b11dab4b0837d3db1cdfcc2c9385d3f3
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-04-28 16:34:28 -0500

    fix the modal padding in small devices

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -38,7 +38,7 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
                 leaveFrom="opacity-100 translate-y-0 sm:sclae-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-4 text-left shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                   <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block z-10">
                     <button
                       type="button"
